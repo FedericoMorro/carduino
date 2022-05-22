2198846ec6010710e7093f8d06a6cbcfaf46c43f
Federico Morro <fedemorro01@gmail.com>
2022-05-22 13:54:46 +0200

    Removed useless parts

--- Main/SmartRobotCarV4.0_V1_20220303/SmartRobotCarV4.0_V1_20220303.ino
+++ Main/SmartRobotCarV4.0_V1_20220303/SmartRobotCarV4.0_V1_20220303.ino
@@ -16,7 +16,6 @@
 void setup()
 {
   // put your setup code here, to run once:
-  //Serial.println("Init");
   Application_FunctionSet.ApplicationFunctionSet_Init();
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   wdt_enable(WDTO_2S);
@@ -26,26 +25,5 @@ void loop()
 {
   //put your main code here, to run repeatedly :
   wdt_reset();
-  Application_FunctionSet.ApplicationFunctionSet_SensorDataUpdate();
-  //Application_FunctionSet.ApplicationFunctionSet_KeyCommand();
-  //Application_FunctionSet.ApplicationFunctionSet_RGB();
-  //Application_FunctionSet.ApplicationFunctionSet_Follow();
   Application_FunctionSet.ApplicationFunctionSet_Obstacle();
-  //Application_FunctionSet.ApplicationFunctionSet_StopWhiteLine();
-  //Application_FunctionSet.ApplicationFunctionSet_Tracking();
-  //Application_FunctionSet.ApplicationFunctionSet_Rocker();
-  //Application_FunctionSet.ApplicationFunctionSet_Standby();
-  //Application_FunctionSet.ApplicationFunctionSet_IRrecv();
-  //Application_FunctionSet.ApplicationFunctionSet_SerialPortDataAnalysis();
-
-  /*
-  Application_FunctionSet.CMD_ServoControl_xxx0();
-  Application_FunctionSet.CMD_MotorControl_xxx0();
-  Application_FunctionSet.CMD_CarControlTimeLimit_xxx0();
-  Application_FunctionSet.CMD_CarControlNoTimeLimit_xxx0();
-  Application_FunctionSet.CMD_MotorControlSpeed_xxx0();
-  Application_FunctionSet.CMD_LightingControlTimeLimit_xxx0();
-  Application_FunctionSet.CMD_LightingControlNoTimeLimit_xxx0();
-  Application_FunctionSet.CMD_ClearAllFunctions_xxx0();
-  */
 }
